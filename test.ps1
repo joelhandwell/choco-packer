0483ca0bcfe2da9559dcded560fd8e399730903d
Stefan Scherer <scherer_stefan@icloud.com>
2015-11-22 11:32:59 +0100

    Reduce file count check

--- test.ps1
+++ test.ps1
@@ -13,7 +13,7 @@ Add-Type -assembly "system.io.compression.filesystem"
 $zip = [IO.Compression.ZipFile]::OpenRead("$pwd\packer.$version.nupkg")
 # Write-Host $zip.Entries.FullName
 Write-Host $zip.Entries.Count
-if ($zip.Entries.Count -ne 43) {
+if ($zip.Entries.Count -ne 5) {
   Write-Error "FAIL: Wrong count in nupkg!"
 }
 $zip.Dispose()
