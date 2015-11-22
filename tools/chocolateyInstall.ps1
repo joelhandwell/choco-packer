814a3ccb65c44b82b44113cf321df41d93b10201
Stefan Scherer <scherer_stefan@icloud.com>
2015-11-22 11:30:38 +0100

    Prepare packer 0.9.0 with single binary

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,12 +1,21 @@
-$url = 'https://dl.bintray.com/mitchellh/packer/packer_0.8.6_windows_386.zip'
-$checksum = '36d485f8368212906560174eacff193be1c76893'
-$url64bit = 'https://dl.bintray.com/mitchellh/packer/packer_0.8.6_windows_amd64.zip'
-$checksum64 = '05c19cc718dd84a9412d990dff309938130ec269'
+#$url = 'https://dl.bintray.com/mitchellh/packer/packer_0.9.0_windows_386.zip'
+$url = 'https://ci.appveyor.com/api/buildjobs/m1b10t5f4owlicn5/artifacts/pkg/windows_386/packer.exe'
+$checksum = 'a16a29b7c08f23b761cb026204c8063bee8c8fe0'
+$checksumType = 'sha1'
+#$url64bit = 'https://dl.bintray.com/mitchellh/packer/packer_0.9.0_windows_amd64.zip'
+$url64bit = 'https://ci.appveyor.com/api/buildjobs/on5cuiw8spjh2gx6/artifacts/pkg/windows_amd64/packer.exe'
+$checksum64 = 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
+$checksumType64 = $checksumType
 $legacyLocation = "$env:SystemDrive\HashiCorp\packer"
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyZipPackage "packer" "$url" "$unzipLocation" "$url64bit" `
- -checksum $checksum -checksumType 'sha1' -checksum64 $checksum64
+# just to prepare the 0.9.0 package we use the AppVeyor artifcacts which are exe and not zip
+$file = "$($unzipLocation)\packer.exe"
+if (![System.IO.Directory]::Exists($unzipLocation)) {[System.IO.Directory]::CreateDirectory($unzipLocation)}
+Get-ChocolateyWebFile $packageName $file $url $url64bit $checksum $checksum64 $checksumType $checksumType64
+
+#Install-ChocolateyZipPackage "packer" "$url" "$unzipLocation" "$url64bit" `
+# -checksum $checksum -checksumType 'sha1' -checksum64 $checksum64
 
 If (Test-Path $legacyLocation) {
   Write-Host "Removing old packer installation from $legacyLocation"
